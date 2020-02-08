089ea2e433a7dbf2378d691dd2366ae81ef8f1df
KevinVanHalen <keviinvanhalen@gmail.com>
2020-02-08 14:57:57 -0600

    SDK PayPal Instalado

--- config.php
+++ config.php
@@ -1,3 +1,12 @@
 <?php 
 
     require 'paypal/autoload.php';
+
+    $apiContext = new \PayPal\Rest\ApiContext(
+        new \PayPal\Auth\OAuthTokenCredential(
+            // ClienteID
+            'AbC63TdVx3H0oaWTMs2ONn6dSE_gYYDcezkQsBTmOEqWd5b6jlcyrbVebEXpT8H84YoMYLVA85iUNHHd',
+            // Secret
+            'EMlMuagBHaNuQrb3tJMMMfWyardRhXb1z1ON80ok1sxRYnzCM-YddBFN09NLgU3ms0esoSt1PfVE7Qk2'
+        )
+    );
